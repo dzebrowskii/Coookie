28be65e0569c92ebea12a66577c695fbdd34702a
Dominik <frexorplay@gmail.com>
2024-02-21 01:18:33 +0100

    commit

--- recipes_management/recipes_management.py
+++ recipes_management/recipes_management.py
@@ -1,21 +1,17 @@
 import pandas as pd
+from myapp.models import Recipe
 
 
-def recipe_searcher(ing):  # recipes management
+def recipe_searcher(ing):
     x = []
     ing = ing.lower().split()
 
-    df = pd.read_csv('recipes.csv')
-    for index, row in df.iterrows():
-        ingredients_list = row['ingredients'].lower().split()
-
-        if any(ingredient in ingredients_list for ingredient in ing) and all(
-                ingredient in ing for ingredient in ingredients_list):
-            x.append(row['description'])
+    for recipe in Recipe.objects.all():
+        ingredients_list = recipe.ingredients.lower().split()
+        if any(ingredient in ingredients_list for ingredient in ing) and all(ingredient in ing for ingredient in ingredients_list):
+            x.append(recipe.description)
 
     if len(x) == 0:
-        text = ['Unfortunately, we have not matched any of the recipes to the given ingredients']
-
-        return text
+        return ['Unfortunately, we have not matched any of the recipes to the given ingredients']
 
     return x
